380ae07072affd1b33a17bc2ca7c3d2870ef1171
rostom17 <rostom825@gmail.com>
2025-10-27 07:13:50 +0600

    utls updated

--- lib/core/utls/retry_logic.dart
+++ lib/core/utls/retry_logic.dart
@@ -1,22 +1,17 @@
 class RetryLogic {
   static const retryableErrorMessages = [
+    'Internet Connection Problem'
     'Connection Timeout.!',
-    "Connection Error",
+    'Connection Error',
     'Unknown Network Error',
     'No internet',
     'SocketException',
     'Network is unreachable',
-    "500: Internal Server Error",
-    "503: Service Unavailable",
+    '500: Internal Server Error',
+    '503: Service Unavailable',
   ];
-
-  static const noInternet = 'Internet Connection Problem';
 
   static bool isRetryable(String errorMessage) {
     return retryableErrorMessages.any((err) => errorMessage.contains(err));
-  }
-
-  static bool isInternetConnectionProblem(String errorMeassage) {
-    return errorMeassage == noInternet;
   }
 }
